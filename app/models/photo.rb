430647e06426145e39d4064d86bff176876c0c47
Roger Vandervort <rvandervort@gmail.com>
2016-07-25 14:18:54 -0400

    Adds basic routes/specs for albums and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,3 @@
 class Photo < ActiveRecord::Base
-  belongs_to :album
+  belongs_to :album, touch: true
 end
---
+++ spec/routes/routing_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "Routing" , type: :routing do
+
+  describe "for albums as a top-level resource" do
+    it { expect(get("/albums")).to route_to("albums#index") }
+    it { expect(get("/albums?page=1")).to route_to(controller: "albums", action: "index", page: "1" ) }
+    it { expect(post("/albums")).to route_to("albums#create") }
+    it { expect(put("/albums/1")).to route_to(controller: "albums", action: "update", id: "1") }
+    it { expect(delete("/albums/1")).to route_to(controller: "albums", action: "destroy", id: "1") }
+
+  end
+
+  describe "for photos as a top-level resource" do
+    it "does not route GET /photos" do 
+      expect(get("/photos")).not_to be_routable
+    end
+
+    it "does not route POST /photos" do 
+      expect(post("/photos")).not_to be_routable
+    end
+
+    it { expect(put("/photos/1")).to route_to(controller: "photos", action: "update", id: "1") }
+    it { expect(delete("/photos/1")).to route_to(controller: "photos", action: "destroy", id: "1") }
+  end
+
+  describe "for photos as a nested resource" do
+    it { expect(get("/albums/123/photos")).to route_to(controller: "photos", action: "index", album_id: "123"
+    ) }
+
+    it { expect(post("/albums/123/photos")).to route_to(controller: "photos", action: "create", album_id: "123")}
+  end
+end
