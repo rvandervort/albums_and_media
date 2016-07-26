9a230e44ce21c484a6be20968fbc3515544cbff5
Roger Vandervort <rvandervort@gmail.com>
2016-07-25 20:11:36 -0400

    GET albums#index returns Album records as json
    no jbuilder views yet, simple model-to-json conversion
    views will be addressed in later commits

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,16 @@
 class AlbumsController < ApplicationController
+  def index
+    respond_to do |format|
+      format.json do
+        result = GetAlbumsService.invoke(page_number: params[:page])
+
+        if result.success?
+          @albums = result[:albums]
+          render json: @albums, status: :ok
+        else
+          render nothing: true, status: :unprocessable_entity
+        end
+      end
+    end
+  end
 end
--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -2,4 +2,30 @@ require 'rails_helper'
 
 RSpec.describe AlbumsController, type: :controller do
 
+  describe 'GET #index' do
+    let(:base_request_attributes) { {format: 'json'} }
+
+    context "for invalid page numbers" do
+      ["-1", "abc", "0"].each do |page_number|
+        it "returns 422 status code when page number is '#{page_number}'" do
+          get :index, base_request_attributes.merge('page' => page_number)
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+    end
+
+    context "for valid page numbers" do
+      let(:request_attributes) { base_request_attributes.merge(page: 2) }
+
+      it "returns 200 ok" do
+        get :index, request_attributes
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "sets provides the resulting list of albums" do
+        get :index, request_attributes
+        expect(assigns(:albums)).to be_a(Album::ActiveRecord_Relation)
+      end
+    end
+  end
 end
