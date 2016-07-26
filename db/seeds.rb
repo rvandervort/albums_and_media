049965ad5b33c4570cd501770b979f22b389baec
Roger Vandervort <rvandervort@gmail.com>
2016-07-26 13:49:00 -0400

    We only need 10 records per

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # Populate albums and photos
-50.times do |album_index|
+10.times do |album_index|
   album = Album.create(name: Faker::Lorem.word.capitalize, position: album_index)
 
-  50.times do |photo_index|
+  10.times do |photo_index|
     album.photos.create(
       name: Faker::Lorem.word.capitalize,
       description: Faker::Lorem.sentence,
