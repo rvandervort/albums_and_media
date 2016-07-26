4ed80157dd1a0fb33276383aeb564931106f00e2
Roger Vandervort <rvandervort@gmail.com>
2016-07-26 11:24:17 -0400

    Adds UpdateAlbumService for updating albums
    Still delegates validation to Album validators ( ##validates_with )

---
+++ app/services/update_album_service.rb
@@ -0,0 +1,47 @@
+class UpdateAlbumService < ServiceBase
+  def execute!
+    if album_record
+      update_album 
+    else
+      invalid_record
+    end
+  end
+
+  private
+
+  def update_album
+    ServiceResult.new.tap do |result|
+      if album_record.update(attributes) 
+        result.success = true
+        result[:album] = album_record
+      else
+        result.success = false
+        result[:errors] = album_record.errors
+      end
+    end
+  end
+
+  def invalid_record
+    ServiceResult.new.tap do |result|
+      result.success = false
+      result[:errors] = ["Album with id #{id} not found"]
+    end
+  end
+
+  
+  def id
+    options[:id]
+  end
+
+  def attributes
+    options[:album]
+  end
+
+  def album_record
+    @album ||= begin
+                  Album.find(id)
+               rescue ActiveRecord::RecordNotFound
+                  nil
+               end
+  end
+end
---
+++ spec/services/update_album_service_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe UpdateAlbumService do
+  describe '#execute' do
+    let(:base_options) { {id: "1", album: {}} }
+
+    let(:service) { described_class.new(options) }
+    let(:result) { service.execute! }
+    let(:model) { Album.new(id: 1123) }
+    
+    context "with an invalid record ID" do
+      let(:options) { base_options }
+      it "returns an unsuccessful result if the album does not exist" do
+        expect(Album).to receive(:find).and_raise(ActiveRecord::RecordNotFound)
+        expect(result).not_to be_success
+        expect(result.errors).not_to be_empty
+      end
+    end
+
+
+    context "with valid album data" do
+      let(:options) { base_options.merge(album: {name: "Test Album"}) }
+      before :each do
+        expect(Album).to receive(:find).and_return(model)
+      end
+
+      it "returns a successful result" do
+        expect(result).to be_success
+      end
+
+      it "returns the album in the :album property" do
+        expect(result[:album]).to eq(model)
+      end
+    end
+
+    context "with invalid album data" do
+      let(:options) { base_options.merge(album: {name: ""}) }
+
+      before :each do
+        expect(Album).to receive(:find).and_return(model)
+      end
+      
+      it "returns an unsuccessful result" do
+        expect(result).not_to be_success
+      end
+
+      it "returns the errors in the :errors property" do
+        expect(result[:errors]).to be_a(ActiveModel::Errors)
+      end
+    end
+  end
+end
