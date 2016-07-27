30177ad7227c94aa277d34efd244f15ac1c48f2a
Roger Vandervort <rvandervort@gmail.com>
2016-07-27 13:42:51 -0400

    Photo model now validates :url with FileExtensionValidator

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,7 @@
 class Photo < ActiveRecord::Base
   belongs_to :album, {touch: true, counter_cache: true}
+
+  include ActiveModel::Validations
+
+  validates_with FileExtensionValidator, fields: :url, extensions: ["jpg", "jpeg"]
 end
